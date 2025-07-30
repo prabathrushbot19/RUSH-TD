cc8f58305035cadadc0203b3c89e2eb08f0acde8
prabathrushbot19 <kanthiriarachchigenandawathe@gmail.com>
2025-07-30 16:45:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `RUSH-TD connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://github.com/prabathrushbot19/RUSH-TD/blob/main/images/RUSH-TD.png?raw=true` }, caption: up })
 
 }
 })
