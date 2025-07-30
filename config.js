1593c49a49e31eca7ec8229e4ae66792327c1c62
prabathrushbot19 <kanthiriarachchigenandawathe@gmail.com>
2025-07-30 16:37:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "mYswxR5b#1PmpROp3WV2c-zp06nOuGLoftiDHHoPXau-FcczewFE",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/prabathrushbot19/RUSH-TD/blob/main/images/Alive.png?raw=true",
-    ALIVE_MSG: process.env.ALIVE_MSG || "╭───〔 🤖 Bot Status 〕───⬣││ 🔹 Bot Name: 🔮 RUSH-TD 🔮│ 🔹 Status: ✅ Online & Active│ 🔹 Ping: 0 ms│ 🔹 Uptime: 00:02[...]"
+    ALIVE_MSG: process.env.ALIVE_MSG || "╭───〔 🤖 Bot Status 〕───⬣││ 🔹 Bot Name: 🔮 RUSH-TD 🔮│ 🔹 Status: ✅ Online & Active│ 🔹 Ping: 0 ms│ 🔹 Uptime: 00:02:25│ 🔹 Platform: linux│ 🔹 Owner: RAMESH DISSANAYAKA│ 🔹 Version: 1.0.0│╰───────────────⬣         🚀 Powered By  RAMESH DISSANAYAKA 🔥"
 };
