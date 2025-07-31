5c4a53099609bd27ccf9c552d6245d03a4e31488
prabathrushbot19 <kanthiriarachchigenandawathe@gmail.com>
2025-07-31 09:04:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "mYswxR5b#1PmpROp3WV2c-zp06nOuGLoftiDHHoPXau-FcczewFE",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/prabathrushbot19/RUSH-TD/blob/main/images/Alive.png?raw=true",
-    ALIVE_MSG: process.env.ALIVE_MSG || '
+    ALIVE_MSG: process.env.ALIVE_MSG || `
 ╔═══◉ *🟢 STATUS: ONLINE* ◉═══╗
 ║  𝙷𝚎𝚢 𝙳𝚞𝚍𝚎, 𝙸’𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚑𝚎𝚕𝚙 𝚢𝚘𝚞.  
 ║  𝙰𝚜𝚔 𝚖𝚎 𝚊𝚗𝚢𝚝𝚑𝚒𝚗𝚐! 💬
@@ -24,6 +24,7 @@ module.exports = {
 
 🚀 Powered By *RAMESH*
 *DISSANAYAKA* 🔥
-       ',  
+`,  
+
     
 };
