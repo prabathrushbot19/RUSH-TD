aa0a2c6c9ecd2692ab1e96e5ec43f82170e14e41
prabathrushbot19 <kanthiriarachchigenandawathe@gmail.com>
2025-07-31 12:55:08 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -9,8 +9,8 @@ cmd({
     category: "download",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins,[...]
-try {
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
 if(!q) return reply("please give me url or title")
 const search = await yts(q)
 const data = search.videos[0]
