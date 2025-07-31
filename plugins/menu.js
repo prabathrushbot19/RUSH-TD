7763b51500b66893edbbd7424257bf5cc98bd5f6
prabathrushbot19 <kanthiriarachchigenandawathe@gmail.com>
2025-07-31 17:29:36 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,63 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "alive",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+downlod: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.legth; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.{commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `*Hello ${pushname}*
+➤ 📥 *DOWNLOAD*
+
+${menu.download}
+
+➤ 💠 *MAIN*
+
+${menu.main}
+
+➤ 👥 *GROUP*
+
+${menu.groups}
+
+➤ 👑 *OWNER*
+
+${menu.owner}
+
+➤ 🔄 *CONVERT*
+
+${menu.convert}
+
+➤ 🔍 *SEARCH*
+
+${menu.search}
+
+┃━━━━━━━━━━━━━━━━━━━━━✦
+┃ ⚙️ Made with ❤️ by
+╰─🔥 *_RAMESH DISSANAYAKA_* 🔥
+`
+  
+await conn.sendMessage(from,{text:madeMenu},{quoted:mek})
+
+  
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
